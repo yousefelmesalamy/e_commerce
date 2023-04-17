a8480bc24d60d28987443c3c337bc09b55d1dda3
yousefelmesalamy <yousefelmesalamy@gmail.com>
2023-04-17 07:32:52 +0200

    .

--- mart/models.py
+++ mart/models.py
@@ -4,9 +4,7 @@ from django.db.models.signals import post_save
 from django.conf import settings
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
-from django_countries.fields import CountryField
-# from cities_light.models import City
-# from cities_light.models import Region
+
 from django.urls import reverse
 from django_rest_passwordreset.signals import reset_password_token_created
 from django.core.mail import send_mail
